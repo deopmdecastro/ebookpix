8d063537d43ddb5fbd6cee86caedd71ac7efe659
deopmdecastro <deopmdecastro@gmail.com>
2023-08-01 21:22:59 +0100

    form

--- php/send-email.php
+++ php/send-email.php
@@ -1,28 +1,46 @@
 <?php
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Replace with your email address
-    $to = "deopmdecastro@gmail.com";
-    $subject = "New Contact Form Submission";
-
-    // Get the form data
-    $name = $_POST["Nome"];
-    $email = $_POST["Email"];
-    $message = $_POST["Mensagem"];
-
-    // Compose the email content
-    $email_content = "Nome: $name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Mensagem:\n$message\n";
-
-    // Set the email headers
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-
-    // Attempt to send the email
-    if (mail($to, $subject, $email_content, $headers)) {
-        echo "Thank you! Your message has been sent.";
-    } else {
-        echo "Oops! Something went wrong. Please try again later.";
+    // Função para limpar e validar os dados enviados pelo formulário
+    function limpar_dados($dados) {
+        $dados = trim($dados);
+        $dados = stripslashes($dados);
+        $dados = htmlspecialchars($dados);
+        return $dados;
     }
+
+    // Recupera os dados enviados pelo formulário e limpa-os
+    $nome = limpar_dados($_POST["nome"]);
+    $email = limpar_dados($_POST["email"]);
+    $mensagem = limpar_dados($_POST["mensagem"]);
+
+    // Verifica se todos os campos foram preenchidos
+    if (empty($nome) || empty($email) || empty($mensagem)) {
+        header("Location: formulario_contato.html?erro=campos_vazios");
+        exit;
+    }
+
+    // Verifica se o e-mail é válido
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: formulario_contato.html?erro=email_invalido");
+        exit;
+    }
+
+    // Resposta automática
+    $assunto = "Obrigado por entrar em conctato, $nome!";
+    $mensagem_auto = "Olá $nome,\n\nObrigado por entrar em contato conosco! Recebemos a seguinte mensagem:\n\n";
+    $mensagem_auto .= "$mensagem\n\n";
+    $mensagem_auto .= "Em breve iremos responder.\n\nAtenciosamente,\nManuel Designer";
+
+    // Cabeçalhos para a resposta automática
+    $cabecalhos = "From: oi@manueldesigner.ao"; // Substitua pelo e-mail da sua empresa
+
+    // Envie a resposta automática
+    mail($email, $assunto, $mensagem_auto, $cabecalhos);
+
+    // Exemplo de redirecionamento para uma página de agradecimento (opcional)
+    header("Location: obrigado.html");
+    exit;
 }
 ?>
